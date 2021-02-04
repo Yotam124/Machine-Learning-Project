c692f8a86f698ed3dbd681858e9418ceae1ca43b
Yotam_d <44746421+Yotam124@users.noreply.github.com>
2021-02-04 20:58:47 +0200

    - cut_to_chunks.py - load_test_set -> updated

--- load_files.py
+++ load_files.py
@@ -47,11 +47,11 @@ def load_test_set(test_path='./IRMAS-TrainingData/'):
         'cello',
         'clarinet',
         'flute',
-        'guitar',
+        'piano',
         'saxophone',
         'trumpet',
         'violin',
-
+        'voice'
     ]
     for root, dirnames, filenames in os.walk(test_path):
         # Audio file
@@ -60,11 +60,11 @@ def load_test_set(test_path='./IRMAS-TrainingData/'):
             filename_class = re.search(r'\[[a-z]+\]', filename).group()
             filename_class[1:4]
             for name in classes:
-                if fnmatch.fnmatchcase(filename, '*' + name + '*'):
+                if fnmatch.fnmatchcase(filename, '' + name[0:3] + ''):
                     test_labels.append(name)
                     break
-            else:
-                test_labels.append('other')
+                # else:
+                #     test_labels.append('other')
         # Text file
         # for filename in fnmatch.filter(filenames, '*.txt'):
         #     f = open(os.path.join(root, filename), 'r')
