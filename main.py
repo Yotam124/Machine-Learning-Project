a5dad0b47553d838f84eced0bd51dd836d509463
mattistern <mattistern1@gmail.com>
2021-02-08 01:11:32 +0200

    marge

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import on_train_classification
 import external_test_classification
+import genre_classification
 
 if __name__ == '__main__':
-    on_train_classification.run()
+    # on_train_classification.run()
     # external_test_classification.run()
+    genre_classification.run('pop_roc')
